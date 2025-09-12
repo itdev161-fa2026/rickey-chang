10de1fcc846135d6267fd925af34f88549f1ce2d
Rickey Chang <changr1@gmatc.matc.edu>
2025-09-12 17:41:01 -0500

    Completed Activity 3

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDatabase from './config/db.js';
+import { check, validationResult } from 'express-validator';
 
 // Initialize express application
 const app = express();
@@ -7,9 +8,34 @@ const app = express();
 // Connect to the Database
 connectDatabase();
 
+// Configure Middleware
+app.use(express.json({ extended: false }));
+
 // API endpoints
+/**
+ * @route GET /
+ * @desc Test endpoint
+ */
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
+);
+
+/**
+ * @route Post api/users
+ * @desc Register user
+ */
+app.post('/api/users', [
+    check('name', 'Please enter your name').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters' ).isLength({ min: 6})
+    ], (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        } else {
+            return res.send(req.body);
+        }
+    }
 );
 
 // Connection Listener
